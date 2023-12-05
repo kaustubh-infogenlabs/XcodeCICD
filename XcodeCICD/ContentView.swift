e3595cd372dd3056d72649821b81dc2845a120fd
Kaustubh Jirapure <kaustubh.j@infogen-labs.com>
2023-12-05 12:31:11 +0530

    Initial Commit

---
+++ XcodeCICD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeCICD
+//
+//  Created by Kaustubh Jirapure on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeCICD/XcodeCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCICDApp.swift
+//  XcodeCICD
+//
+//  Created by Kaustubh Jirapure on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
